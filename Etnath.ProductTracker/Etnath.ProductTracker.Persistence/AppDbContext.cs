46ca16c9fcb83efc75e744f8137b376ab4abb5d5
Etnath <kevin.giffone@gmail.com>
2016-11-27 16:46:17 +0100

    Add Product to DAL Add AppDbContext to Persistence layer

---
+++ Etnath.ProductTracker/Etnath.ProductTracker.Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Etnath.ProductTracker.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etnath.ProductTracker.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products;
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
