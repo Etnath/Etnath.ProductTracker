80e8d730976ae9e7abd8595b939f2aa2579d999c
Etnath <kevin.giffone@gmail.com>
2016-11-27 15:36:08 +0100

    Add base projects

---
+++ Etnath.ProductTracker/Etnath.ProductTracker.Presentation.Shell/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Etnath.ProductTracker.Presentation.Shell.Views;
+using System.Windows;
+
+namespace Etnath.ProductTracker.Presentation.Shell
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
